d1cc826f25afd077958bfea79c10c5fbf070dd26
lanou3g <lanou3g@lanou3gdeMac-mini-6.local>
2015-07-20 14:52:05 +0800

    sasfaf

---
+++ myRepository/myRepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myRepository
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 guofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
